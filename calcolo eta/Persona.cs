5338c10a0c267b7749eda3db42cf0f3baae8cf18
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-24 12:39:26 +0100

    Aggiungere i file di progetto.

---
+++ calcolo eta/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcolo_eta
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+
+        public DateTime DataNascita { get; set; }
+
+        public int Eta
+        { get
+            { int eta = DateTime.Today.Year - DataNascita.Year;
+                if (DateTime.Today.Month > DataNascita.Month)
+                { return eta; }
+                else if (DateTime.Today.Month == DataNascita.Month)
+                {
+                    if (DateTime.Today.Day >= DataNascita.Day)
+                    {
+                        return eta;
+                    }
+                    else { return eta - 1; }
+                    }
+                //monthoggi<monthdata
+                return eta-1;
+                }
+        }
+
+    }
+}
---
+++ calcolo eta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcolo_eta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.DataNascita = new DateTime();
+            p.DataNascita = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine(p.Eta);
+        }
+    }
+}
